708d4cbbecaed9a2f11bd42cf24b4d090ef1e01e
zloy92 <zloy_92@mai.ru>
2024-03-06 19:34:05 +0500

    version4

--- flutter_application_1/lib/screens/quiz/components/body.dart
+++ flutter_application_1/lib/screens/quiz/components/body.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_application_1/constants.dart';
-import 'package:flutter_application_1/models/Questions.dart';
+
 import 'package:flutter_application_1/screens/quiz/components/progress_bar.dart';
+import 'package:flutter_application_1/screens/quiz/components/question_card.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -46,71 +47,6 @@ class Body extends StatelessWidget {
           ),
         )
       ],
-    );
-  }
-}
-
-class QuestionCard extends StatelessWidget {
-  const QuestionCard({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(kDefaultPadding),
-      decoration: BoxDecoration(
-          color: Colors.white, borderRadius: BorderRadius.circular(25)),
-      child: Column(
-        children: [
-          Text(
-            sample_data[0]['question'],
-            style: Theme.of(context)
-                .textTheme
-                .headline6
-                ?.copyWith(color: kBlackColor),
-          ),
-          Option(),
-          Option(),
-          Option(),
-          Option(),
-        ],
-      ),
-    );
-  }
-}
-
-class Option extends StatelessWidget {
-  const Option({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: kDefaultPadding),
-      padding: EdgeInsets.all(kDefaultPadding),
-      decoration: BoxDecoration(
-        border: Border.all(color: kGrayColor),
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            "1. Test",
-            style: TextStyle(color: kGrayColor, fontSize: 16),
-          ),
-          Container(
-            height: 26,
-            width: 26,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50),
-              border: Border.all(color: kGrayColor),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
--- flutter_application_1/lib/screens/quiz/components/question_card.dart
+++ flutter_application_1/lib/screens/quiz/components/question_card.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/constants.dart';
 import 'package:flutter_application_1/models/Questions.dart';
 
+import 'package:flutter_application_1/screens/quiz/components/option.dart';
+
 class QuestionCard extends StatelessWidget {
   const QuestionCard({
     super.key,
@@ -9,10 +11,26 @@ class QuestionCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      sample_data[0]['question'],
-      style:
-          Theme.of(context).textTheme.headline6?.copyWith(color: kBlackColor),
+    return Container(
+      padding: EdgeInsets.all(kDefaultPadding),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(25)),
+      child: Column(
+        children: [
+          Text(
+            sample_data[0]['question'],
+            style: Theme.of(context)
+                .textTheme
+                .headline6
+                ?.copyWith(color: kBlackColor),
+          ),
+          SizedBox(height: kDefaultPadding / 2),
+          Option(),
+          Option(),
+          Option(),
+          Option(),
+        ],
+      ),
     );
   }
 }
