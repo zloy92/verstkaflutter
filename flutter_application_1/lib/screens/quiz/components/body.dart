4231b0750106238d1aa6f60501c7d102a72205ef
zloy92 <zloy_92@mai.ru>
2024-03-06 14:33:24 +0500

    version2

--- flutter_application_1/lib/screens/quiz/components/body.dart
+++ flutter_application_1/lib/screens/quiz/components/body.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_application_1/constants.dart';
-import 'package:websafe_svg/websafe_svg.dart';
+import 'package:flutter_application_1/models/Questions.dart';
+import 'package:flutter_application_1/screens/quiz/components/progress_bar.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -14,7 +15,74 @@ class Body extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
             child: Column(
-              children: [ProgressBar()],
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ProgressBar(),
+                SizedBox(height: kDefaultPadding),
+                Text.rich(
+                  TextSpan(
+                    text: "Question 1",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline4
+                        ?.copyWith(color: kSecondaryColor),
+                    children: [
+                      TextSpan(
+                          text: "/10",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline5
+                              ?.copyWith(color: kSecondaryColor)),
+                    ],
+                  ),
+                ),
+                Divider(
+                  thickness: 1.5,
+                ),
+                SizedBox(height: kDefaultPadding),
+                Container(
+                  padding: EdgeInsets.all(kDefaultPadding),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(25)),
+                  child: Column(
+                    children: [
+                      Text(
+                        sample_data[0]['question'],
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6
+                            ?.copyWith(color: kBlackColor),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: kDefaultPadding),
+                        padding: EdgeInsets.all(kDefaultPadding),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: kGrayColor),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "1. Test",
+                              style: TextStyle(color: kGrayColor, fontSize: 16),
+                            ),
+                            Container(
+                              height: 26,
+                              width: 26,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                border: Border.all(color: kGrayColor),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         )
